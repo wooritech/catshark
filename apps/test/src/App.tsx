3c66d7ceffc109fb77d3c2b08ee5916031be46fb
Malloc72P <scra1028@gmail.com>
2023-01-04 13:40:43 +0900

    임시 커밋

---
+++ apps/test/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import {Button} from '@wooritech/catshark-core'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Button></Button>
+    </div>
+  )
+}
+
+export default App
